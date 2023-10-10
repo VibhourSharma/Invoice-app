7c9aad7d3455c2422c0afccc9c9d067f855955fe
Vibhour Sharma <vibhour121@gmail.com>
2023-10-10 22:10:57 +0530

    🚀dark mode functionality added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import logo from "../../public/assets/logo.png";
 import moon from "../../public/assets/icon-moon.svg";
+import sun from "../../public/assets/icon-sun.svg";
 import profile from "../../public/assets/image-avatar.jpg";
 import "./../Index.css";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <div className="flex relative">
       <div className="fixed h-full bg-sidebar rounded-t-3xl rounded-br-3xl">
@@ -11,7 +27,12 @@ const Header = () => {
           <img src={logo} alt="app-logo" />
         </div>
         <div className="absolute bottom-32 w-full flex justify-center">
-          <img src={moon} alt="dark-mode" className="cursor-pointer p-1" />
+          <img
+            src={theme === "dark" ? sun : moon}
+            alt="dark-mode"
+            className="cursor-pointer p-1"
+            onClick={handleTheme}
+          />
         </div>
         <div className="w-full h-px bg-slate-400 absolute bottom-24"></div>
         <div className="w-full flex justify-center absolute bottom-0 py-6">
