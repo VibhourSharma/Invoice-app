74cc4b8f6f336c85e636a66cdf78c0fd41003581
Vibhour Sharma <vibhour121@gmail.com>
2023-10-20 22:41:56 +0530

    home page created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import InvoiceCard from "../components/InvoiceCard";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <InvoiceCard />
+    </>
+  );
+};
+
+export default Home;
