d3aa0326530d2f795579b90d0dcff983d8453293
Vibhour Sharma <vibhour121@gmail.com>
2023-10-25 23:57:54 +0530

    🎨styling the receipt section

--- src/pages/Receipt.jsx
+++ src/pages/Receipt.jsx
@@ -1,100 +1,32 @@
 import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
 import data from "../Data";
+import leftIcon from "../assets/icon-arrow-left.svg";
 console.log(data);
 
 const Receipt = () => {
   const { id } = useParams();
-  const receiptData = data.filter((item) => {
+  const receiptData = data.find((item) => {
     return item.id === id;
   });
-
-  const receiptDataD = receiptData[0];
 
   return (
     <>
       <Navbar />
       <div className="dark:bg-[#141625] dark:text-white bg-[#F2F2F2] flex min-h-screen justify-center font-def tracking-tighter">
-        <h2>Receipt ID: {receiptDataD.id}</h2>
-        <p>Name: {receiptDataD.clientName}</p>
-        {/* <p>Description: {receiptData.description}</p> */}
+        <div className="m-20 w-1/2 flex flex-col">
+          <div className="w-fit">
+            <div className="flex items-center font-bold text-base">
+              <div className="flex items-center justify-center mr-6">
+                <img src={leftIcon} alt="icon" className="w-full" />
+              </div>
+              Go back
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Receipt;
-
-// clientAddress
-// :
-// city
-// :
-// "Sharrington"
-// country
-// :
-// "United Kingdom"
-// postCode
-// :
-// "NR24 5WQ"
-// street
-// :
-// "106 Kendell Street"
-// [[Prototype]]
-// :
-// Object
-// clientEmail
-// :
-// "jensenh@mail.com"
-// clientName
-// :
-// "Jensen Huang"
-// createdAt
-// :
-// "2021-8-18"
-// description
-// :
-// "Re-branding"
-// id
-// :
-// "RT3080"
-// items
-// :
-// Array(1)
-// 0
-// :
-// {name: 'Brand Guidelines', quantity: 1, price: 1800.9, total: 1800.9}
-// length
-// :
-// 1
-// [[Prototype]]
-// :
-// Array(0)
-// paymentDue
-// :
-// "2021-8-19"
-// paymentTerms
-// :
-// 1
-// senderAddress
-// :
-// city
-// :
-// "London"
-// country
-// :
-// "United Kingdom"
-// postCode
-// :
-// "E1 3EZ"
-// street
-// :
-// "19 Union Terrace"
-// [[Prototype]]
-// :
-// Object
-// status
-// :
-// "paid"
-// total
-// :
-// 1800.9
