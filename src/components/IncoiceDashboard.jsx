b4a16554db7a12a4c572a7473cf5342927c2fdc1
Vibhour Sharma <vibhour121@gmail.com>
2023-10-26 22:45:46 +0530

    components renamed⚙️

--- src/components/IncoiceDashboard.jsx
+++ src/components/IncoiceDashboard.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import downIcon from "../../public/assets/icon-arrow-down.svg";
-import plusIcon from "../../public/assets/plus.png";
-import rightArrow from "../../public/assets/icon-arrow-right.svg";
-import checkIcon from "../../public/assets/icon-check.svg";
+import downIcon from "../assets/icon-arrow-down.svg";
+import plusIcon from "../assets/plus.png";
+import rightArrow from "../assets/icon-arrow-right.svg";
 import data from "../Data";
+import { Link } from "react-router-dom";
 
-const InvoiceCard = () => {
+const InvoiceDashboard = () => {
   const statusColors = {
     pending: "bg-orange-50 text-[#ff8f00]",
     paid: "bg-[#e7fff4] text-[#33D69F]",
@@ -42,7 +42,7 @@ const InvoiceCard = () => {
           <div>
             <h1 className="text-4xl font-bold">Invoices</h1>
             <p className="text-base text-slate-500">
-              There are a total {data.length} Invoices
+              There are a total {filteredData.length} Invoices
             </p>
           </div>
           <div className="flex items-center justify-between w-72">
@@ -59,7 +59,6 @@ const InvoiceCard = () => {
                   <label className="flex items-center gap-2">
                     <input
                       type="checkbox"
-                      name="draft"
                       className="ml-4 w-4 h-4 cursor-pointer"
                       onChange={(e) => filter(e)}
                       checked={selectedFilters.includes("draft")}
@@ -72,7 +71,6 @@ const InvoiceCard = () => {
                   <label className="flex items-center gap-2">
                     <input
                       type="checkbox"
-                      name="pending"
                       className="relative ml-4 w-4 h-4 cursor-pointer"
                       value="pending"
                       checked={selectedFilters.includes("pending")}
@@ -85,7 +83,6 @@ const InvoiceCard = () => {
                   <label className="flex items-center gap-2">
                     <input
                       type="checkbox"
-                      name="paid"
                       className="ml-4 w-4 h-4 cursor-pointer"
                       checked={selectedFilters.includes("paid")}
                       onChange={(e) => filter(e)}
@@ -105,36 +102,35 @@ const InvoiceCard = () => {
 
         {/* Mapping over the data  */}
         {filteredData.map((d) => (
-          <div
-            key={d.id}
-            className="flex rounded-lg h-24 bg-white mt-4 justify-between dark:bg-[#1E2139] hover:border hover:border-[#7C5DFA] cursor-pointer"
-          >
-            <div className="flex items-center">
-              <span className="font-bold mr-12 ml-8">
-                <span className="text-slate-500">#</span>
-                {d.id}
-              </span>
-              <span className="text-slate-500 mr-12">{d.createdAt}</span>
-              <span className="text-slate-500">{d.clientName}</span>
-            </div>
-
-            {/* right side  */}
-            <div className="flex items-center">
-              <span className="font-bold text-xl mr-12">£ {d.total}</span>
-              <div
-                className={`dark:bg-opacity-5 font-semibold rounded-lg flex items-center justify-center w-20 p-0.5 mr-12 ${
-                  statusColors[d.status]
-                } ${d.status === "draft" ? "dark:text-white" : ""}`}
-              >
-                <span className="text-3xl">•</span> {d.status}
+          <Link to={`/InvoiceDashboard/receipt/${d.id}`} key={d.id}>
+            <div className="flex rounded-lg h-24 bg-white mt-4 justify-between dark:bg-[#1E2139] hover:border hover:border-[#7C5DFA] cursor-pointer">
+              <div className="flex items-center">
+                <span className="font-bold mr-12 ml-8">
+                  <span className="text-slate-500">#</span>
+                  {d.id}
+                </span>
+                <span className="text-slate-500 mr-12">{d.createdAt}</span>
+                <span className="text-slate-500">{d.clientName}</span>
               </div>
-              <img src={rightArrow} alt=">" className="mr-8" />
+
+              {/* right side  */}
+              <div className="flex items-center">
+                <span className="font-bold text-xl mr-12">£ {d.total}</span>
+                <div
+                  className={`dark:bg-opacity-5 font-semibold rounded-lg flex items-center justify-center w-20 p-0.5 mr-12 ${
+                    statusColors[d.status]
+                  } ${d.status === "draft" ? "dark:text-white" : ""}`}
+                >
+                  <span className="text-3xl">•</span> {d.status}
+                </div>
+                <img src={rightArrow} alt=">" className="mr-8" />
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
   );
 };
 
-export default InvoiceCard;
+export default InvoiceDashboard;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import logo from "../assets/logo.png";
+import moon from "../assets/icon-moon.svg";
+import sun from "../assets/icon-sun.svg";
+import profile from "../assets/image-avatar.jpg";
+import "./../Index.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <div className="flex relative">
+      <Link>
+        <div className="fixed h-full bg-sidebar rounded-t-3xl rounded-br-3xl">
+          <div>
+            <img src={logo} alt="app-logo" />
+          </div>
+          <div className="absolute bottom-32 w-full flex justify-center">
+            <img
+              src={theme === "dark" ? sun : moon}
+              alt="dark-mode"
+              className="cursor-pointer p-1"
+              onClick={handleTheme}
+            />
+          </div>
+          <div className="w-full h-px bg-slate-400 absolute bottom-24"></div>
+          <div className="w-full flex justify-center absolute bottom-0 py-6">
+            <img src={profile} alt="profile" className="rounded-full h-11" />
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
