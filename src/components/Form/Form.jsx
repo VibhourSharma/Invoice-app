e8b14ea58c860a622b01bb6a846b353d465343d4
Vibhour Sharma <vibhour121@gmail.com>
2024-02-13 00:00:06 +0530

    ⚒️New: Assigned random id and created date

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,13 +2,65 @@ import { useState } from "react";
 import AdditionalInfo from "./AdditionalInfo";
 import BillFrom from "./BillFrom";
 import BillTo from "./BillTo";
+import data from "../../Data";
 
 const Form = ({ onClose }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [randomID, setRandomID] = useState("");
+
+  const generateID = () => {
+    const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let randomAlphabet1 =
+      alphabets[Math.floor(Math.random() * alphabets.length)];
+    let randomAlphabet2 =
+      alphabets[Math.floor(Math.random() * alphabets.length)];
+
+    const numbers = "1234567890";
+    let randomNumber1 = numbers[Math.floor(Math.random() * numbers.length)];
+    let randomNumber2 = numbers[Math.floor(Math.random() * numbers.length)];
+    let randomNumber3 = numbers[Math.floor(Math.random() * numbers.length)];
+    let randomNumber4 = numbers[Math.floor(Math.random() * numbers.length)];
+
+    setRandomID(
+      `${randomAlphabet1}${randomAlphabet2}${randomNumber1}${randomNumber2}${randomNumber3}${randomNumber4}`
+    );
   };
 
-  const [formData, setFormData] = useState({});
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split("T")[0];
+
+  const values = {
+    id: `${randomID}`,
+    createdAt: formattedDate,
+    paymentDue: "",
+    description: "",
+    paymentTerms: 1,
+    clientName: "",
+    clientEmail: "",
+    status: "",
+    senderAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: "",
+    },
+    clientAddress: {
+      street: "",
+      city: "",
+      postCode: "",
+      country: "",
+    },
+    items: [
+      {
+        name: "",
+        quantity: 0,
+        price: 0,
+        total: 0,
+      },
+    ],
+    total: 0,
+  };
+
+  const [formData, setFormData] = useState(values);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,6 +68,13 @@ const Form = ({ onClose }) => {
       ...prevData,
       [name]: value,
     }));
+    console.log(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    generateID();
+    localStorage.setItem("formData", JSON.stringify(formData));
   };
 
   return (
@@ -45,6 +104,7 @@ const Form = ({ onClose }) => {
             <button
               type="submit"
               className="bg-[#7C5DFA] text-white h-12 w-32 rounded-3xl hover:bg-[#8e72fc] text-sm font-semibold"
+              onClick={handleSubmit}
             >
               Save & Send
             </button>
