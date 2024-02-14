3a14d1b8bda82f85689bf7fc688e4044251e1a5e
Vibhour Sharma <vibhour121@gmail.com>
2024-02-14 22:57:22 +0530

    ⚒️New: Form drawer refactored

---
+++ src/components/FormDrawer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Form from "./Form/Form";
+
+const FormDrawer = ({ isOpen, onClose }) => {
+  const backdropStyle = {
+    display: isOpen ? "block" : "none",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  };
+
+  const drawerStyle = {
+    transform: isOpen ? "translateX(0)" : "translateX(-120%)",
+    overflowY: "auto",
+    transition: "transform 0.3s ease-in-out",
+  };
+
+  return (
+    <>
+      <div style={backdropStyle} onClick={onClose} />
+      <div
+        className="fixed top-0 left-0 w-[40rem] h-full bg-white ease-in-out delay-200 duration-300 shadow-2xl ml-24 rounded-r-3xl"
+        style={drawerStyle}
+      >
+        <Form onClose={onClose} />
+      </div>
+    </>
+  );
+};
+
+export default FormDrawer;
